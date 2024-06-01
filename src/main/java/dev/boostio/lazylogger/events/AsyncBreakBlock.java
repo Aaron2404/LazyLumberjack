89884d505a76f2d781b68552fe3df4733ddb061e
Bram <bram@bramdekker.com>
2024-06-01 23:20:39 +0200

    Fixed: User being retrieved in a for loop

--- src/main/java/dev/boostio/lazylogger/events/AsyncBreakBlock.java
+++ src/main/java/dev/boostio/lazylogger/events/AsyncBreakBlock.java
@@ -1,12 +1,12 @@
 package dev.boostio.lazylogger.events;
 
 import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.protocol.player.User;
 import com.github.retrooper.packetevents.util.Vector3i;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerBlockBreakAnimation;
 import dev.boostio.lazylogger.LazyLogger;
 import dev.boostio.lazylogger.managers.LogManager;
 import io.github.retrooper.packetevents.util.folia.FoliaScheduler;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -31,42 +31,45 @@ public class AsyncBreakBlock implements Listener {
     @EventHandler
     public void onAsyncBreakBlock(BlockBreakEvent event) {
         Player player = event.getPlayer();
+        User user = PacketEvents.getAPI().getPlayerManager().getUser(player);
+
         Material mainHandItem = player.getInventory().getItemInMainHand().getType();
         Material blockType = event.getBlock().getType();
 
-        if (player.isSneaking() && player.hasPermission("LazyLogger.Use") && logManager.isAxe(mainHandItem) && logManager.isLog(blockType)) {
-            List<Block> oakLogs = new ArrayList<>();
-            logManager.findRelatedLogs(event.getBlock(), oakLogs, new HashSet<>(), 0, 0);
-            Material logMaterial = oakLogs.get(0).getType();
+        if (!player.isSneaking() || !player.hasPermission("LazyLogger.Use") || !logManager.isAxe(mainHandItem) || !logManager.isLog(blockType)) {
+            return;
+        }
 
-            int counter = 0;
-            for (Block block : oakLogs) {
-                if(!logManager.isLog(block.getType())){
-                    continue;
-                }
+        List<Block> oakLogs = new ArrayList<>();
+        logManager.findRelatedLogs(event.getBlock(), oakLogs, new HashSet<>(), 0, 0);
+        Material logMaterial = oakLogs.get(0).getType();
 
-                counter++;
-
-                FoliaScheduler.getRegionScheduler().runDelayed(this.plugin, block.getLocation(), (o) -> {
-                    for (byte i = 0; i < 9; i++) {
-                        final byte stage = i;
-
-                        FoliaScheduler.getRegionScheduler().runDelayed(this.plugin, block.getLocation(), (o1) -> {
-                            player.sendMessage("Breaking block at stage " + stage);
-
-                            PacketEvents.getAPI().getPlayerManager().sendPacket(player, new WrapperPlayServerBlockBreakAnimation(player.getEntityId(), new Vector3i(block.getX(), block.getY(), block.getZ()), stage));
-                        }, 1L * i);
-
-                        FoliaScheduler.getRegionScheduler().runDelayed(this.plugin, block.getLocation(), (o2) -> {
-                            block.breakNaturally();
-                        }, 1L * i);
-                    }
-                }, 1L * 8 * counter);
+        int counter = 0;
+        for (Block block : oakLogs) {
+            if (!logManager.isLog(block.getType())) {
+                continue;
             }
 
-            logManager.findLowestY(oakLogs).stream()
-                    .filter(log -> logManager.isDirtOrPodzol(log.getRelative(BlockFace.DOWN).getType()))
-                    .forEach(log -> logManager.plantSapling(log, logMaterial));
+            counter++;
+
+            FoliaScheduler.getRegionScheduler().runDelayed(this.plugin, block.getLocation(), (o) -> {
+                for (byte i = 0; i < 9; i++) {
+                    final byte stage = i;
+
+                    FoliaScheduler.getRegionScheduler().runDelayed(this.plugin, block.getLocation(), (o1) -> {
+                        player.sendMessage("Breaking block at stage " + stage);
+
+                        user.sendPacket(new WrapperPlayServerBlockBreakAnimation(player.getEntityId(), new Vector3i(block.getX(), block.getY(), block.getZ()), stage));
+                    }, i);
+                    FoliaScheduler.getRegionScheduler().runDelayed(this.plugin, block.getLocation(), (o2) -> {
+                        block.breakNaturally();
+                    }, i);
+                }
+            }, (long) 8 * counter);
         }
+
+        logManager.findLowestY(oakLogs).stream()
+                .filter(log -> logManager.isDirtOrPodzol(log.getRelative(BlockFace.DOWN).getType()))
+                .forEach(log -> logManager.plantSapling(log, logMaterial));
     }
 }
